57e891227bd91e708cd7d63b829dd7590cca7194
__frantisek_petko <oristgrey@gmail.com>
2024-10-03 15:44:40 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import "./css-w3.css";
 import "./js/email-toast";
 import "./js/mobile-menu";
 import "./js/page-loading";
 import "./js/ticket-purchase";
 import "./style.css";
-import "./w3.css";
 
